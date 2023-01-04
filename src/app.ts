453ebf075126cf9841d4231e053a6d09ba69e504
Anfeed <truonganvu2000@gmail.com>
2023-01-04 22:15:02 +0700

    set up cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "config";
+import cors from "cors";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
@@ -11,6 +12,7 @@ const host = config.get("host") as string;
 const app = express();
 app.use("/api", deserializeUser);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
