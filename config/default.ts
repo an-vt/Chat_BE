ff33a90683cca39c6496975ac5df11080d98325d
Anfeed <truonganvu2000@gmail.com>
2023-01-10 23:00:02 +0700

    feature - update-name-database: rename database

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 export default {
   port: 1337,
   host: "localhost",
-  dbUri: "mongodb://localhost:27017/rest-api",
+  dbUri: "mongodb://localhost:27017/chat",
   saltWorkFactor: 10,
   accessTokenTtl: "30m",
   refreshTokenTtl: "1y",
